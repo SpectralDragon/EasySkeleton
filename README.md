95ea47bf3fa2b384d68b02e413574594885e4096
Владислав Прусаков <v.prusakov@corp.mail.ru>
2022-10-27 18:06:34 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EasySkeleton
+
+A description of this package.
---
+++ Tests/EasySkeletonTests/EasySkeletonTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import EasySkeleton
+
+final class EasySkeletonTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(EasySkeleton().text, "Hello, World!")
+    }
+}
